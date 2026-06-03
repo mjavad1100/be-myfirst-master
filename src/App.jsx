b3d9757361966cdd80f3b76a05f9eee34d958f85
jvd <m.javadkhodsiani@gmail.com>
2026-06-03 21:46:02 +0330

    RUN THIS SHIT PLZ

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,59 @@
-import About from './section/about/About';
-import 'bootstrap/dist/css/bootstrap-grid.css';
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/react"
-import Resume from './section/resume/resume';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
 
+import { FaHome, FaUser, FaFolderOpen } from 'react-icons/fa';
+
+import About from './section/about/About';
+import Resume from './section/resume/resume';
+import Services from './section/services/Services';
+
+import 'bootstrap/dist/css/bootstrap-grid.css';
+
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
-    return (
-        <div>
-            <main>
-            <SpeedInsights />
-            <Analytics/>
-                <About />
-                <Resume />
-            </main>
+  return (
+    <Router>
+
+      <SpeedInsights />
+      <Analytics />
+
+      {/* ULTRA ICON DOCK MENU */}
+
+      <nav className="global__menu">
+
+        <div className="global__links">
+
+          <Link to="/" className="global__link">
+            <FaHome />
+          </Link>
+
+          <Link to="/resume" className="global__link">
+            <FaUser />
+          </Link>
+
+          <Link to="/services" className="global__link">
+            <FaFolderOpen />
+          </Link>
+
         </div>
-    );
+
+      </nav>
+
+      {/* ROUTES */}
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+
+    </Router>
+  );
 };
 
 export default App;
