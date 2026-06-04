b37b07dcfbecce59f18eb52f78e747dea36ab7f2
jvd <m.javadkhodsiani@gmail.com>
2026-06-05 02:07:47 +0330

    plz

--- src/section/about/data.js
+++ src/section/about/data.js
@@ -8,13 +8,13 @@ import { FaTelegramPlane } from 'react-icons/fa';
 const data = [
   {
     id: 1,
-    link: 'https://instagram.com/YOUR_USERNAME',
+    link: 'https://www.instagram.com/amirkashanik?igsh=eDJjemoxbTZueWVm',
     icon: <AiOutlineInstagram />,
   },
 
   {
     id: 2,
-    link: 'https://t.me/YOUR_USERNAME',
+    link: 'https://t.me/Amir_kash',
     icon: <FaTelegramPlane />,
   },
 
